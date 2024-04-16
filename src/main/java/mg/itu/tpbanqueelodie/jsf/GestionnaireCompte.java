8a4fe16b7f0837f529de4cdeba5cfdab432a6418
Andriamah <andriamahanintsoelo@gmail.com>
2024-04-16 13:17:03 +0200

    Écriture d'un bean CDI GestionnaireCompte

---
+++ src/main/java/mg/itu/tpbanqueelodie/jsf/GestionnaireCompte.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpbanqueelodie.jsf;
+
+import jakarta.inject.Named;
+
+/**
+ *
+ * @author andri
+ */
+@Named("gestionnaireCompte")
+public class GestionnaireCompte {
+    
+}
